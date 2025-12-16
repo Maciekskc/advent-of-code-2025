f5ee15396d417c1e3ee25ef044a1fa523f1ac473
Maciej Białkowski <maciekskc@gmail.com>
2025-12-16 13:03:21 +0100

    feat: Implement solution for Day04 Part 1

---
+++ AdventOfCode/Day04/Program.cs
@@ -0,0 +1,81 @@
+﻿using Helpers;
+
+var path = "input.txt";
+
+using var filestream = FileHelper.GetFileStream(path);
+var magazine = FileHelper.GetCharMatrixFromFile(filestream);
+filestream.Close();
+
+var answer = new PrintingDepartmentSolver(magazine).FindAccessibleRolls();
+Console.WriteLine($"The answer for given input is {answer}");
+
+internal class PrintingDepartmentSolver(char[][] magazine, bool isDebugEnabled = false)
+{
+    const char RollChar = '@';
+    const char AccesibleRollChar = 'x';
+    const char EmptySlot = '.';
+    const int MaxRollNeighbours = 4;
+
+    internal int FindAccessibleRolls()
+    {
+        for (var i = 0; i < magazine.Length; i++)
+        for (var j = 0; j < magazine[i].Length; j++)
+        {
+            if (magazine[i][j] == EmptySlot)
+                continue;
+
+            if (IfAccessible(i, j))
+                magazine[i][j] = AccesibleRollChar;
+        }
+
+        PrintMagazine();
+        return CountAccessibleRolls();
+    }
+
+    private int CountAccessibleRolls() => magazine.SelectMany(row => row).Count(column => column == AccesibleRollChar);
+
+    private bool IfAccessible(int x, int y)
+    {
+        var counter = 0;
+        if (isDebugEnabled)
+            Console.WriteLine($"Check for Magazine[{x}][{y}] is a roll . Incrementing.");
+
+        for (var i = x - 1; i <= x + 1; i++)
+        {
+            if (i < 0 || i >= magazine.Length)
+                continue;
+            for (var j = y - 1; j <= y + 1; j++)
+            {
+                if (i == x && j == y)
+                    continue;
+
+                if (j < 0 || j >= magazine[i].Length)
+                    continue;
+                if (magazine[i][j] == RollChar || magazine[i][j] == AccesibleRollChar)
+                {
+                    if (isDebugEnabled)
+                        Console.WriteLine($"Magazine[{i}][{j}] is a roll . Incrementing.");
+                    counter++;
+                }
+            }
+        }
+
+        if (isDebugEnabled)
+            Console.WriteLine($"Magazine[{x}][{y}] have {counter} roll neighbours.");
+
+        return counter < MaxRollNeighbours;
+    }
+
+    private void PrintMagazine()
+    {
+        foreach (var line in magazine)
+        {
+            foreach (var character in line)
+            {
+                Console.Write(character);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AdventOfCode/Helpers/FileHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace Helpers;
+
+public static class FileHelper
+{
+    public static char[][] GetCharMatrixFromFile(StreamReader sr)
+    {
+        List<char[]> matrix = [];
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            ArgumentException.ThrowIfNullOrEmpty(line);
+            matrix.Add(line.ToCharArray());
+        }
+
+        return matrix.ToArray();
+    }
+
+    public static StreamReader GetFileStream(string path)
+    {
+        if (!File.Exists(path)) throw new ArgumentException($"File {path} does not exist.");
+        return new StreamReader(path);
+    }
+}
