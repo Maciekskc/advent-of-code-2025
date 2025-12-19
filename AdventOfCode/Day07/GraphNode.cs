0ca27ef765e808e1f414381ce3fc2b9b0bbba96c
Maciej Białkowski <maciekskc@gmail.com>
2025-12-19 16:25:51 +0100

    feat: Work in progress on part 2. Currently - inoptimal solution for the problem size, using DSF as an algorithm. Working on solution interpolating DP.

---
+++ AdventOfCode/Day07/GraphNode.cs
@@ -0,0 +1,22 @@
+﻿namespace Day07;
+
+public class GraphNode(int x, int y, List<GraphNode> parents)
+{
+    public int PosX { get; init; } = x;
+    public int PosY { get; init; } = y;
+    public List<GraphNode> Parents { get; set; } = parents;
+    public List<GraphNode> Childs { get; set; } = [];
+
+    public int NumberOfSubgraphs()
+    {
+        if (Childs.Count == 0)
+            return 1;
+        int total = 0;
+        foreach (var child in Childs)
+        {
+            total += child.NumberOfSubgraphs();
+        }
+
+        return total;
+    }
+}
--- AdventOfCode/Day07/Program.cs
+++ AdventOfCode/Day07/Program.cs
@@ -1,76 +1,108 @@
-﻿using Helpers;
+﻿using System.Diagnostics;
+using Day07;
+using Helpers;
 
-var path = "input.txt";
+var path = "input-test.txt";
 var lines = FileHelper.GetFileStream(path).GetStringListFromFile();
 
 var solver = new LaboratoriesSolver(lines);
-Console.WriteLine($"The answer for given input is {solver.AnalyseBeamSplit()}");
+Console.WriteLine($"The answer for given input is {solver.AnalyseQuantumBeamSplit()}");
 
-internal class LaboratoriesSolver(string[] lines)
+internal partial class LaboratoriesSolver(string[] lines)
 {
     private int _splitCounter = 0;
+    private int _timeLineCounter = 0;
     
-    public int AnalyseBeamSplit()
+    public int AnalyseQuantumBeamSplit()
     {
-        var linesWithBeamAdapted = new List<string>() { lines[0] };
-        var beamPositions = new List<int> { lines[0].IndexOf('S') };
-
-        foreach (var line in lines[1..])
-        {
-            var ( propagatedLine, newBeamPositions ) = PropagateBeamToTheNextRow(line, beamPositions);
-            linesWithBeamAdapted.Add(propagatedLine);
-            beamPositions = newBeamPositions;
-        }
-
-        PrintBeam(linesWithBeamAdapted);
+        _timeLineCounter = 0;
         
-        return _splitCounter;
-    }
+        Stopwatch sw = Stopwatch.StartNew();
+        Console.WriteLine($"Quantum calculation need to consider whole multiverse. Please give it some time to finish...");
 
-    private (string line, List<int> newBeamPositions) PropagateBeamToTheNextRow(string nextLine, List<int> beamPositions)
-    {
-        var lineChars = nextLine.ToCharArray();
-        var newBeamPositions = new List<int>();
-        foreach (var beamPosition in beamPositions)
+        if (false)
         {
-            var doesSplit = false;
-            var characterAtBeamPosition = nextLine[beamPosition];
-            if (characterAtBeamPosition == '^')
-            {
-                // Beam splits into two
-                if (beamPosition - 1 >= 0 && lineChars[beamPosition - 1] == '.')
-                {
-                    doesSplit = true;
-                    lineChars[beamPosition - 1] = '|';
-                    newBeamPositions.Add(beamPosition - 1);
-                }
-
-                if (beamPosition + 1 <= lineChars.Length && lineChars[beamPosition + 1] == '.')
-                {
-                    doesSplit = true;
-                    lineChars[beamPosition + 1] = '|';
-                    newBeamPositions.Add(beamPosition + 1);
-                }
-            }
-            else
-            {
-                lineChars[beamPosition] = '|';
-                newBeamPositions.Add(beamPosition);
-            }
-            if (doesSplit)
-                _splitCounter++;
+            //First try with DFS recursion. Inoptimal in the input case.
+            var currentLines = lines[..0] ;
+            var beamPosition = lines[0].IndexOf('S');
+            var currentLevel = 1;
+            var maxLevel = lines.Length;
+            RecursiveQuantumLevelCalculation(lines, currentLines, beamPosition, currentLevel, maxLevel);
         }
-        Console.WriteLine($"{new string(lineChars)} - Splits: {_splitCounter}");
-        return (new string(lineChars), newBeamPositions);
+        else
+        {
+            var fullDiagram = AnalyseBeamSplit().finallMatrix;
+            var graph = CompileDiagramGraph(fullDiagram);
+            PrintGraph(graph);
+            _timeLineCounter = graph[0].NumberOfSubgraphs();
+        }
+        sw.Stop();
+        Console.WriteLine($"Job done. It took {sw.Elapsed.Seconds} seconds.");
+
+        
+        return _timeLineCounter;
     }
-    
+
+    private List<GraphNode> CompileDiagramGraph(string[] lines)
+    {
+        var graphNodes = new List<GraphNode>();
+        for (var y = 0; y < lines.Length; y++)
+        {
+            var line = lines[y];
+            // Console.WriteLine($"[{y:D2}]{line}");
+            for (var x = 0; x < line.Length; x++)
+            {
+                var character = line[x];
+                if (character == '^')
+                {
+                    var parentNodes = graphNodes
+                        .Where(n => n.PosY < y && (n.PosX == x - 1 || n.PosX == x + 1))
+                        .GroupBy(node => node.PosX)
+                        .Select(g => g.OrderByDescending(n => n.PosY).First())
+                        .ToList();
+                    var currentNode = new GraphNode(x, y, parentNodes);
+                    foreach (var parent in parentNodes)
+                        parent.Childs.Add(currentNode);
+
+                    graphNodes.Add(currentNode);
+                    // Console.WriteL}ine($"[{y:D2}]{lines[y].Replace('^','.')}[({x},{y}) parents: {string.Join(",",parentNodes.Select(n => $"({n.PosX},{n.PosY})"))}]");
+                    
+                    
+                }
+                
+                if (y == lines.Length -1)
+                    if(line[x] == '|')
+                    {
+                        var parentNodes = graphNodes
+                            .Where(n => n.PosY < y && n.PosX == x)
+                            .GroupBy(node => node.PosX)
+                            .Select(g => g.OrderByDescending(n => n.PosY).First())
+                            .ToList();
+                        var currentNode = new GraphNode(x, y, parentNodes);
+                        foreach (var parent in parentNodes)
+                            parent.Childs.Add(currentNode);
+
+                        graphNodes.Add(currentNode);
+                        Console.WriteLine($"[{y:D2}]{lines[y].Replace('|','.')}[({x},{y}) parents: {string.Join(",",parentNodes.Select(n => $"({n.PosX},{n.PosY})"))}]");
+                    }
+            }
+        }
+        return graphNodes;
+    }
+
+
     private static void PrintBeam(List<string> lines)
     {
         Console.WriteLine("BEAMS:");
         foreach (var line in lines)
             Console.WriteLine(line);
     }
-    
-    
+
+    private void PrintGraph(List<GraphNode> nodes){
+        foreach (var node in nodes)
+        {
+            Console.WriteLine($"Node at ({node.PosX},{node.PosY}) with parents: {string.Join(",",node.Parents.Select(n => $"({n.PosX},{n.PosY})"))} and childs: {string.Join(",",node.Childs.Select(n => $"({n.PosX},{n.PosY})"))}");
+        }
+    }
 }
 
