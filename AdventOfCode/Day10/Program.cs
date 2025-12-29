fa66f3d7cbc3790ecb3cd91528e15593deaa58d8
Maciej Białkowski <macb@netcompany.com>
2025-12-29 10:54:09 +0100

    feat: Implement solution for Day10 part 1

--- AdventOfCode/Day10/Program.cs
+++ AdventOfCode/Day10/Program.cs
@@ -1,51 +1,109 @@
 ﻿using Helpers;
 
-var path = "input-test.txt";
+var path = "input.txt";
 var lines = FileHelper.GetFileStream(path).GetStringListFromFile();
 
-foreach (var line in lines)
-    Console.WriteLine(line);
-
 var solver = new FactorySolver(lines);
-// Console.WriteLine($"The answer for given input is {solver.}");
+Console.WriteLine($"The answer for given input is {solver.SolvePuzzles()}");
 
 internal class FactorySolver
 {
     private List<Machine> _machines = [];
+
     public FactorySolver(string[] lines)
     {
         foreach (var line in lines)
-        {
             _machines.Add(new Machine(line));
+    }
+
+    public long SolvePuzzles()
+    {
+        long sum = 0;
+        foreach (var machine in _machines)
+        {
+            var requiredClicks = FindMinimalButtonClickCountToGetToTheLightsTargetSequence(machine);
+            Console.WriteLine(machine);
+            Console.WriteLine($"To get to the initiation state you need {requiredClicks} clicks");
+            sum += requiredClicks;
         }
+
+        return sum;
+    }
+
+    private int FindMinimalButtonClickCountToGetToTheLightsTargetSequence(Machine machine)
+    {
+        var combinationLength = 1;
+        do
+        {
+            
+            var combinations = Numerics.NET.Iterators.Combinations(machine.Buttons.Length, combinationLength);
+            foreach (var combination in combinations)
+            {
+                var currentSequence = machine.SimulateButtonsSequenceClick(combination);
+                if (currentSequence == machine.IndicatorTargetSequence)
+                    return combinationLength;
+            }
+
+            combinationLength++;
+            
+            if(combinationLength == 20)
+            {
+                Console.WriteLine("We reach length 20. It is unexpectedly big. Terminating");
+                return -1;
+            }
+        } while (true); //it has to find at some point
     }
 
     private class Machine
     {
-        private int _range = -1;
-        private readonly string _indicatorTargetSequence;
-        private string _currentSequence;
-        private int[][] indicatorTriggeringLights;
+        public readonly string IndicatorTargetSequence;
+        public int[][] Buttons { get; }
 
         public Machine(string machineDescription)
         {
-            string[] inputs = machineDescription.Split(' ');
-            _indicatorTargetSequence = inputs[0][1..^1];
-            _currentSequence = new string('.', _indicatorTargetSequence.Length);
+            var inputs = machineDescription.Split(' ');
+            IndicatorTargetSequence = inputs[0][1..^1];
 
-            indicatorTriggeringLights = new int[_indicatorTargetSequence.Length][];
-            int iterator = 0;
+            Buttons = new int[inputs.Length - 2][];
+            var iterator = 0;
             foreach (var input in inputs[1..^1])
             {
                 var digits = input[1..^1].Split(',');
-                indicatorTriggeringLights[iterator] = new int[digits.Length];
+                Buttons[iterator] = new int[digits.Length];
                 for (var index = 0; index < digits.Length; index++)
                 {
-                    indicatorTriggeringLights[iterator][index] = int.Parse(digits[index]);
+                    Buttons[iterator][index] = int.Parse(digits[index]);
                 }
 
                 iterator++;
             }
+        }
+
+        public override string ToString() =>
+            $"Machine with initiating sequence [{IndicatorTargetSequence}]. Buttons [{string.Join(",", Buttons.Select(button => "(" + string.Join(",", button) + ")"))}]";
+
+        public string SimulateButtonsSequenceClick(int[] buttonsSequence)
+        {
+            var lights = new char[IndicatorTargetSequence.Length];
+            Array.Fill(lights, '.');
+
+            foreach (var buttonIndex in buttonsSequence)
+            {
+                PressButton(buttonIndex, lights);
+            }
+
+            return new string(lights);
+        }
+
+        private void PressButton(int buttonIndex, char[] lights)
+        {
+           foreach (var lightIndex in Buttons[buttonIndex])
+           {
+                if (lightIndex < 0 || lightIndex >= lights.Length)
+                    continue;
+
+                lights[lightIndex] = lights[lightIndex] == '.' ? '#' : '.';
+           }
         }
     }
 }
